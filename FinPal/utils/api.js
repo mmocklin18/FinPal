fbedf29b414da367702beaf10b03c990a5a2d59a
Matt Mocklin <matt.mocklin@gmail.com>
2025-05-04 20:34:38 -0400

    added axios request interceptor to include JWT token from AsyncStorage

---
+++ FinPal/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const api = axios.create({
+    baseURL: "http://localhost:5001/api",
+});
+
+//On every request in /api, if token is in aysnc storage add to header
+api.interceptors.request.use(async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+
+
+});
